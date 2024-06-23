6961cbe376c0923954505fe548e14cd10d655cad
Prasanna Kumar <kumar22maran@gmail.com>
2024-06-23 23:02:02 +0530

    Added routes

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ import {Router} from "express";
 import AuthController from "../controller/AuthController.js";
 import {authMiddleware} from "../middleware/Authenticate.js";
 import ProfileController from "../controller/ProfileController.js";
+import NewsController from "../controller/NewsController.js";
 
 const router = Router();
 
@@ -14,5 +15,12 @@ router.post("/login/",AuthController.login)
 
 router.get("/profile",authMiddleware,ProfileController.index)
 router.put("/profile/:id",ProfileController.update)
+
+
+router.get("/news",NewsController.index)
+router.post("/news",authMiddleware,NewsController.store)
+router.get("/news/:id",NewsController.show)
+router.put("/news/:id",NewsController.update)
+router.delete("/news/:id",NewsController.delete)
 
 export default router
